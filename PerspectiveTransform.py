6bd12c484c963b7573fb0fe26279350eead35d1c
arthur autohandle <autohandle@yahoo.com>
2017-08-02 23:37:19 +0000

    backup

--- PerspectiveTransform.py
+++ PerspectiveTransform.py
@@ -1,11 +1,16 @@
 import numpy as np
 import cv2
 
-fullImagePerspectiveTransforms= {'./test_images/straight_lines1.jpg': np.array([[ -5.31244816e-01,  -1.51726430e+00,   9.84704530e+02],
-       [ -1.82076576e-14,  -1.63799355e+00,   7.18279930e+02],
-       [ -3.03576608e-17,  -2.35939109e-03,   1.00000000e+00]]), './test_images/straight_lines2.jpg': np.array([[ -5.44776959e-01,  -1.50649420e+00,   9.77714734e+02],
-       [ -1.19904087e-14,  -1.65418663e+00,   7.20006876e+02],
-       [ -1.99493200e-17,  -2.38024185e-03,   1.00000000e+00]])}
+fullImagePerspectiveTransforms= {'./test_images/straight_lines1.jpg': 
+                                 np.array([
+                                     [ -5.31244816e-01,  -1.51726430e+00,   9.84704530e+02],
+                                     [ -1.82076576e-14,  -1.63799355e+00,   7.18279930e+02],
+                                     [ -3.03576608e-17,  -2.35939109e-03,   1.00000000e+00]]),
+                                 './test_images/straight_lines2.jpg': np.array([
+                                     [ -5.44776959e-01,  -1.50649420e+00,   9.77714734e+02],
+                                     [ -1.19904087e-14,  -1.65418663e+00,   7.20006876e+02],
+                                     [ -1.99493200e-17,  -2.38024185e-03,   1.00000000e+00]])
+                                }
 # TOPOFTRAPEZOID=.4
 fullImagePerspectiveTransforms= {'./test_images/straight_lines2.jpg': np.array([[ -5.44706987e-01,  -1.50703683e+00,   9.78066903e+02],
        [ -6.43929354e-15,  -1.76349840e+00,   7.90995625e+02],
@@ -13,22 +18,44 @@ fullImagePerspectiveTransforms= {'./test_images/straight_lines2.jpg': np.array([
        [ -7.99360578e-15,  -1.74486685e+00,   7.87582977e+02],
        [ -1.12757026e-17,  -2.35952813e-03,   1.00000000e+00]])}
 # TOPOFTRAPEZOID=.6
-fullImagePerspectiveTransforms= {'./test_images/straight_lines1.jpg': np.array([[ -5.31513375e-01,  -1.51949207e+00,   9.86150355e+02],
-       [ -3.99680289e-15,  -1.85192138e+00,   8.56944796e+02],
-       [ -6.93889390e-18,  -2.35980489e-03,   1.00000000e+00]]), './test_images/straight_lines2.jpg': np.array([[ -5.44565668e-01,  -1.50812703e+00,   9.78774441e+02],
-       [ -3.55271368e-15,  -1.87266770e+00,   8.61938220e+02],
-       [ -6.50521303e-18,  -2.37991628e-03,   1.00000000e+00]])}
+fullImagePerspectiveTransforms= {'./test_images/straight_lines1.jpg': 
+                                 np.array([
+                                     [ -5.31513375e-01,  -1.51949207e+00,   9.86150355e+02],
+                                     [ -3.99680289e-15,  -1.85192138e+00,   8.56944796e+02],
+                                     [ -6.93889390e-18,  -2.35980489e-03,   1.00000000e+00]]),
+                                 './test_images/straight_lines2.jpg': 
+                                 np.array([
+                                     [ -5.44565668e-01,  -1.50812703e+00,   9.78774441e+02],
+                                     [ -3.55271368e-15,  -1.87266770e+00,   8.61938220e+02],
+                                     [ -6.50521303e-18,  -2.37991628e-03,   1.00000000e+00]])}
 print("fullImagePerspectiveTransforms:", fullImagePerspectiveTransforms, ", fullImagePerspectiveTransforms.keys:", fullImagePerspectiveTransforms.keys())
+fullImageInverseTransforms= {'./test_images/straight_lines2.jpg': 
+                             np.array([
+                                 [  1.75205780e-01,  -8.05336297e-01,   5.22663257e+02],
+                                 [ -1.11022302e-16,  -5.33997399e-01,   4.60272863e+02],
+                                 [ -2.38524478e-18,  -1.27086937e-03,   1.00000000e+00]]),
+                             './test_images/straight_lines1.jpg':
+                             np.array([
+                                 [  1.73013823e-01,  -8.20494982e-01,   5.32501243e+02],
+                                 [  1.22124533e-15,  -5.39979734e-01,   4.62732819e+02],
+                                 [  2.81892565e-18,  -1.27424681e-03,   1.00000000e+00]])
+                            }
+print("fullImageInverseTransforms:", fullImageInverseTransforms, ", fullImageInverseTransforms.keys:", fullImageInverseTransforms.keys())
 
 PERSPECTIVETRANSFORM=fullImagePerspectiveTransforms[list(fullImagePerspectiveTransforms.keys())[1]]
 print("PERSPECTIVETRANSFORM:", PERSPECTIVETRANSFORM)
+INVERSETRANSFORM=fullImageInverseTransforms[list(fullImageInverseTransforms.keys())[1]]
+print("INVERSETRANSFORM:", INVERSETRANSFORM)
 
 SCALEX=1
 SCALEY=1
 
-def transform(undistortedRgbImage):
+def transform(undistortedRgbImage, perspectiveTransform=PERSPECTIVETRANSFORM):
     targetImageSize=(SCALEX*undistortedRgbImage.shape[1], SCALEY*undistortedRgbImage.shape[0])
-    return cv2.warpPerspective(undistortedRgbImage, PERSPECTIVETRANSFORM, targetImageSize, flags=cv2.INTER_LINEAR)
+    return cv2.warpPerspective(undistortedRgbImage, perspectiveTransform, targetImageSize, flags=cv2.INTER_LINEAR)
+
+def unwarp(distortedRgbImage):
+    return transform(distortedRgbImage, INVERSETRANSFORM)
 
 import UndistortImage
 def undistortAndTransform(rgbImage):
@@ -39,9 +66,11 @@ def testUndistortAndTransform():
     testImageName='./test_images/test1.jpg'
     print("testImageName:", testImageName)
     testRgbImage=mpimage.imread(testImageName)
-    undistortedImage=undistortAndTransform(testRgbImage)
+    warpedImage=undistortAndTransform(testRgbImage)
     print("testImageName: ",testImageName, ", testRgbImage.shape:", testRgbImage.shape, ", type:", testRgbImage.dtype)
-    print("testImageName: ",testImageName, ", undistortedImage.shape:", undistortedImage.shape, ", type:", undistortedImage.dtype)
+    print("testImageName: ",testImageName, ", warpedImage.shape:", warpedImage.shape, ", type:", warpedImage.dtype)
+    unwarpedImage=unwarp(testRgbImage)
+    print("testImageName: ",testImageName, ", unwarpedImage.shape:", unwarpedImage.shape, ", type:", unwarpedImage.dtype)
 
-#print ("dir:", dir(),", dir(UndistortImage):", dir(UndistortImage))
-#testUndistortAndTransform()
+print ("dir:", dir(),", dir(UndistortImage):", dir(UndistortImage))
+testUndistortAndTransform()
--- ProcessImage.py
+++ ProcessImage.py
@@ -7,6 +7,8 @@ TRIMSTARTINGROWS=(1./10.)
 TRIMENDINGROWS=(1./5.)
 TRIMSTARTINGCOLUMNS=(1./10.)
 
+MINPIX=50
+
 def locateLaneMarkerIndex(binaryImage):
     print("locateLaneMarkerIndex - binaryImage.shape:", binaryImage.shape, ", type:", binaryImage.dtype)
     startingInRow=int(binaryImage.shape[0]*TRIMSTARTINGROWS)
@@ -43,9 +45,9 @@ def initializeSlidingWindows(binaryImage):
     print("total pixels:", binaryImage.shape[0]*binaryImage.shape[1], ", whiteX:", len(whiteX), ", whiteY:", len(whiteY))
 
     # Set the width of the windows +/- margin
-    margin = 100
+    # margin = 100
     # Set minimum number of pixels found to recenter window
-    minpix = 50
+    # minpix = 50
 
     # Create empty lists to receive left and right lane pixel indices
     left_lane_inds = []
@@ -62,7 +64,7 @@ def initializeSlidingWindows(binaryImage):
     #midpoint = np.int(histogram.shape[0]/2)
     #leftx_base = np.argmax(histogram[:midpoint])
     #rightx_base = np.argmax(histogram[midpoint:]) + midpoint
-    print("leftx_base:", leftx_base, ", rightx_base:", rightx_base)
+    print("initializeSlidingWindows-leftx_base:", leftx_base, ", rightx_base:", rightx_base)
     
     # Current positions to be updated for each window
     leftx_current = leftx_base
@@ -81,7 +83,7 @@ def initializeSlidingWindows(binaryImage):
     # Set the width of the windows +/- margin
     margin = 100
     # Set minimum number of pixels found to recenter window
-    minpix = 50
+    # minpix = 50
 
     # Create empty lists to receive left and right lane pixel indices
     left_lane_inds = []
@@ -91,12 +93,12 @@ def initializeSlidingWindows(binaryImage):
     nwindows = 9
     # Set height of windows
     window_height = np.int(binaryImage.shape[0]/nwindows)
-    print("nwindows: ",nwindows, ", window_height:", window_height)
+    print("initializeSlidingWindows-nwindows: ",nwindows, ", window_height:", window_height)
 
-    print("binaryImage.shape:", binaryImage.shape, ", type:", binaryImage.dtype)
+    print("initializeSlidingWindows-binaryImage.shape:", binaryImage.shape, ", type:", binaryImage.dtype)
     visualizationImage = np.dstack((binaryImage, binaryImage, binaryImage))*255
     #visualizationImage = croppedTestImages[testImageName]
-    print("visualizationImage.shape:", visualizationImage.shape, ", type:", visualizationImage.dtype)
+    print("initializeSlidingWindows-visualizationImage.shape:", visualizationImage.shape, ", type:", visualizationImage.dtype)
 
     # Step through the windows one by one
     for window in range(nwindows):
@@ -126,9 +128,9 @@ def initializeSlidingWindows(binaryImage):
         right_lane_inds.append(good_right_inds)
         
         # If you found > minpix pixels, recenter next window on their mean position
-        if len(good_left_inds) > minpix:
+        if len(good_left_inds) > MINPIX:
             leftx_current = np.int(np.mean(whiteX[good_left_inds]))
-        if len(good_right_inds) > minpix:        
+        if len(good_right_inds) > MINPIX:        
             rightx_current = np.int(np.mean(whiteX[good_right_inds]))
 
     # Concatenate the arrays of indices
@@ -162,23 +164,27 @@ def processInitialImage(binaryImage):
     #p.imshow(visualizationImage)
     return [left_fit, right_fit], visualizationImage
    
+MARGIN = 100
 
 def processImage(binaryImage, left_fit, right_fit):
     # Assume you now have a new warped binary image 
     # from the next frame of video (also called "binary_warped")
     # It's now much easier to find line pixels!
     nonzero = binaryImage.nonzero() # [y-row,x-col] that are not 0 (i.e. 1)
-    nonzeroy = np.array(nonzero[0])
-    print("nonzeroy.shape:", nonzeroy.shape, ", type:", nonzeroy.dtype)
-    nonzerox = np.array(nonzero[1])
-    print("nonzerox.shape:", nonzerox.shape, ", type:", nonzerox.dtype)
-    margin = 100
+    nonzeroy = np.array(nonzero[0]) # only the nonzero y
+    print("processImage-nonzeroy.shape:", nonzeroy.shape, ", type:", nonzeroy.dtype)
+    nonzerox = np.array(nonzero[1]) # only the nonzero x
+    print("processImage-nonzerox.shape:", nonzerox.shape, ", type:", nonzerox.dtype)
+    #margin = 100
     left_laneX = left_fit[0]*nonzeroy**2 + left_fit[1]*nonzeroy + left_fit[2]
-    print("left_laneX.shape:", left_laneX.shape, ", type:", left_laneX.dtype)
+    print("processImage-left_laneX.shape:", left_laneX.shape, ", type:", left_laneX.dtype)
     # find x values between margins in the new frame using the old polynomial
-    left_lane_inds = ((nonzerox > (left_laneX - margin)) & (nonzerox < (left_laneX + margin))) 
+    left_lane_inds = ((nonzerox > (left_laneX - MARGIN)) & (nonzerox < (left_laneX + MARGIN))) 
     right_laneX = right_fit[0]*nonzeroy**2 + right_fit[1]*nonzeroy + right_fit[2]
-    right_lane_inds = ((nonzerox > (right_laneX - margin)) & (nonzerox < (right_laneX + margin)))  
+    right_lane_inds = ((nonzerox > (right_laneX - MARGIN)) & (nonzerox < (right_laneX + MARGIN)))
+    print("processImage-left_lane_inds.shape:", left_lane_inds.shape, ", type:", left_lane_inds.dtype, ", counts:", np.unique(left_lane_inds, return_counts=True))
+    print("processImage-right_lane_inds.shape:", right_lane_inds.shape, ", type:", right_lane_inds.dtype, ", counts:", np.unique(right_lane_inds, return_counts=True))
+   
 
     # extract corresponding left and right line pixel positions that fall within old 2nd order poly margin
     leftx = nonzerox[left_lane_inds]
@@ -209,15 +215,15 @@ def testProcessImage():
     videoImages= ['./test_images/video_frames/frame0001.jpg',    './test_images/video_frames/frame0002.jpg']
     for videoFrameName, videoFrameIndex in zip(videoImages, range(0, len(videoImages))):
         videoFrame=mpimage.imread(videoFrameName)
-        print("videoFrameName: ",videoFrameName, ", videoFrame.shape:", videoFrame.shape, ", type:", videoFrame.dtype)
+        print("testProcessImage-videoFrameName: ",videoFrameName, ", videoFrame.shape:", videoFrame.shape, ", type:", videoFrame.dtype)
         binaryImage,incrementalImages=EnhanceLaneMarkers.enhanceLaneMarkers(videoFrame, trimLeft=TRIMLEFT, trimTop=TRIMTOP, trimBottom=TRIMBOTTOM)
-        print("videoFrameName: ",videoFrameName, ", binaryImage.shape:", binaryImage.shape, ", type:", binaryImage.dtype)
+        print("testProcessImage-videoFrameName: ",videoFrameName, ", binaryImage.shape:", binaryImage.shape, ", type:", binaryImage.dtype)
     
         if ploty==None :
             [left_fit, right_fit], visualizationImage=processInitialImage(binaryImage)
         else: # process a new frame by assuming the old 2nd order polynomial still fits the new frame
             [left_fit, right_fit], visualizationImage=processImage(binaryImage, left_fit, right_fit)
-        print("left_fit: ",left_fit, ", right_fit:", right_fit)
-        print("videoFrameName: ",videoFrameName, ", visualizationImage.shape:", visualizationImage.shape, ", type:", visualizationImage.dtype)
+        print("testProcessImage-left_fit: ",left_fit, ", right_fit:", right_fit)
+        print("testProcessImage-videoFrameName: ",videoFrameName, ", visualizationImage.shape:", visualizationImage.shape, ", type:", visualizationImage.dtype)
         
 testProcessImage()
